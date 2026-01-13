31f169ce5f59ea7fa103889976354a1d388f0a35
SamLee347 <157442273+SamLee347@users.noreply.github.com>
2026-01-13 10:24:39 +0800

    Add project files.

---
+++ Pages/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment1.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
